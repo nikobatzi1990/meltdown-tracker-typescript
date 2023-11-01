64b8a49ec0b2977ede6c6c9cc15a42d7fd00a01f
nikobatzi1990 <nicole.boci@gmail.com>
2023-11-01 22:29:11 +0900

    wip: create backend server

---
+++ server/index.ts
@@ -0,0 +1,15 @@
+const setUpServer = require('./server');
+const PORT = process.env.PORT || 8000;
+const server = setUpServer();
+const knex = require('../database/knex');
+
+(async () => {
+  try {
+    await knex.migrate.latest();
+    server.listen(PORT, () => {
+      console.log(`app is listening @ http://localhost:${PORT}`);
+    })
+  } catch (err) {
+    console.error(`app failed to start: ${err}`);
+  }
+})();
---
+++ server/server.ts
@@ -0,0 +1,3 @@
+const express = require('express');
+const knex = require('../database/knex');
+const path = require('path');
