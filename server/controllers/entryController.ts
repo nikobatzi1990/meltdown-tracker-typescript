c8ad4c20cdbf2db51af9446facde7e6a1cd193ec
nikobatzi1990 <nicole.boci@gmail.com>
2023-11-20 18:43:19 +0900

    chore: abstract endpoints related to entries

--- server/controllers/entryController.ts
+++ server/controllers/entryController.ts
@@ -19,8 +19,40 @@ router.get('/:uid', async (req: Request, res: Response) => {
   }
 });
 
+// endpoint for getting one entry by id
+router.get('/:uid/entry/:entryId', async (req: Request, res: Response) => {
+  console.log(req.params);
+  try {
+    const entry = await knex
+      .from('posts')
+      .join('tag_to_post', 'tag_to_post.post_id', '=','posts.id')
+      .join('tags', 'tag_to_post.tag_id', '=', 'tags.id')
+      .select('title', 'body', 'time_of_day', 'flagged', 'tags.tag_name', 'created_at', 'intensity')
+      .where('posts.id', '=', req.params.entryId);
+      res.status(200).send(entry[0])
+
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// endpoint for all entries that include specified tag
+router.get('/api/entries/:tagName', async (req: Request, res: Response) => {
+  try {
+    const result = await knex.select('title', 'body')
+    .from('posts')
+    .join('tag_to_post', 'tag_to_post.post_id', '=','posts.id')
+    .join('tags', 'tag_to_post.tag_id', '=', 'tags.id')
+    .where('tags.tag_name', '=', req.params.tagName);
+    res.status(200).send(result);
+
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
 // endpoint for posting a new entry submission
-router.post(':/uid/submission', async (req: Request, res: Response) => {
+router.post('/:uid/submission', async (req: Request, res: Response) => {
   const { tagName, timesUsed, title, body, timeOfDay, flagged, intensity } = req.body;
   const uid = req.params.uid
   const userId = await knex.select('id')
@@ -52,6 +84,41 @@ router.post(':/uid/submission', async (req: Request, res: Response) => {
         'post_id': postQuery[0].id
       });
     res.status(200).send(postQuery)
+
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// endpoint for editing an entry by id
+router.patch('/:uid/:entryId/edit', async (req: Request, res: Response) => {
+  const { title, body, timeOfDay, flagged, intensity } = req.body;
+  try {
+    await knex('posts')
+      .where('posts.id', '=', req.params.entryId)
+      .update({
+      'title': title, 
+      'body': body,
+      'time_of_day': timeOfDay,
+      'flagged': flagged,
+      'updated_at': new Date(),
+      'intensity': intensity
+    });
+    res.status(200).send("Post Edited!")
+
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// endpoint for deleting an entry
+router.delete('/:uid/:entryId/deletion', async (req: Request, res: Response) => {
+  const id = req.params.entryId;
+  try {
+    await knex('posts')
+      .delete()
+      .where('posts.id', id);
+    res.status(200).send("Post deleted")
 
   } catch (error) {
     res.status(400).send(error);
