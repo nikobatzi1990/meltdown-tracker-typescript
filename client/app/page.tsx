98ff3397a9402a8e726678fe4d59f194931f642f
nikobatzi1990 <nicole.boci@gmail.com>
2023-12-02 22:13:07 +0900

    fix: incorrect routing structure

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Link from 'next/link';
 import Image from 'next/image';
 
 export default function Home() {
@@ -15,7 +16,7 @@ export default function Home() {
       <div className="flex min-h-screen flex-col items-center justify-between p-24">
         <Header />
         <main >
-          
+          <Link href='/login'>Login</Link>
         </main>
         <Footer />
       </div>
