df8d4b96b5fd2e037620cc1d6384268f0c45ad11
nikobatzi1990 <nicole.boci@gmail.com>
2023-12-05 00:09:15 +0900

    chore: create user auth context

--- client/app/context/AuthContext.tsx
+++ client/app/context/AuthContext.tsx
@@ -5,49 +5,79 @@ import {
   signInWithEmailAndPassword, 
   createUserWithEmailAndPassword, 
   signOut,
-  onAuthStateChanged 
+  onAuthStateChanged,
+  UserCredential 
 } from 'firebase/auth';
 
-interface CurrentUser {
+interface User {
   username: string,
   email: string, 
   uid: string | null
+};
+
+interface AuthenticatedUser {
+  signup: (username: string, email: string, password: string) => Promise<UserCredential | undefined>,
+  login: (email: string, password: string) => Promise<UserCredential | undefined>,
+  logout: () => Promise<void>,
+  user: User | null
 }
 
-const UserContext = createContext<CurrentUser | null>(null);
+const UserContext = createContext<AuthenticatedUser | null>(null);
 
 export const AuthContextProvider = ({children}: { children: ReactNode }) => {
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState<User | null>(null);
 
-  const createUser = async (username: string, email: string, password: string) => {
-    const newUserInfo = {
+  const signup = async (username: string, email: string, password: string) => {
+    const newUserInfo: User = {
       username: username,
       email: email,
-      password: password,
       uid: ''
     };
 
-    const newUser = await createUserWithEmailAndPassword(auth, email, password);
-    newUserInfo.uid = newUser.user.uid;
-    await axios.post('/api/signup', newUserInfo);
-    return newUser;
+    try {
+      const newUser = await createUserWithEmailAndPassword(auth, email, password);
+      newUserInfo.uid = newUser.user.uid;
+      await axios.post('/api/signup', newUserInfo);
+      return newUser;
+    } catch (err) {
+      console.error("Signup failed:", err);
+      throw err;
+    }
   };
 
-  const loginUser = async (email: string, password: string) => {
-    const loggedIn = signInWithEmailAndPassword(auth, email, password);
-    return loggedIn;
+  const login = async (email: string, password: string) => {
+    try {
+      const loggedIn = await signInWithEmailAndPassword(auth, email, password);
+      return loggedIn;
+    } catch (err) {
+      console.error("Login failed:", err);
+      throw err;
+    }
   };
 
-  const logoutUser = () => {
+  const logout = () => {
     return signOut(auth);
   };
 
   useEffect(() => {
     const authenticatedUser = onAuthStateChanged(auth, (currentUser) => {
       if (currentUser) {
-        setUser(currentUser);
+        const { displayName, email, uid} = currentUser;
+        setUser({
+          username: displayName || '',
+          email: email || '',
+          uid: uid || null,
+        });
       }
     });
       return authenticatedUser;
   }, []);
+
+  return <UserContext.Provider value={{ signup, login, logout, user }}>
+    {children}
+  </UserContext.Provider>
+};
+
+export const UserAuth = () => {
+  return useContext(UserContext);
 }
