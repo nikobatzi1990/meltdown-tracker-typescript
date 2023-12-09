ba0f3ae1440028ce447145e0df523ee48167eaff
nikobatzi1990 <nicole.boci@gmail.com>
2023-12-09 21:19:47 +0900

    feat: create Entrylist component

--- client/app/components/EntryList.tsx
+++ client/app/components/EntryList.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { UserAuth } from "../context/AuthContext";
 import axios from 'axios';
 import { HiLightBulb } from "react-icons/hi";
+import Link from 'next/link';
 
 interface Entries {
   id: number,
@@ -13,7 +14,7 @@ interface Entries {
 
 export default function EntryList() {
   const auth = UserAuth();
-  const currentUser = auth?.user
+  const currentUser = auth?.user;
   const [entries, setEntries] = useState<Entries[]>([]);
 
   const handleEntries = async () => {
@@ -33,8 +34,20 @@ export default function EntryList() {
   
   return (
     <>
-
+      { entries.length > 0
+      ? entries.map((entry) => (
+          <div 
+            key={entry.id} 
+          >
+            <Link href="#">{entry.title}</Link>
+              { entry.flagged &&
+                <HiLightBulb title="This was a significant event!" alt="Significant Event" />
+              }
+            <p>{entry.body}</p>
+          </div>
+        ))
+      : "You haven't added any entries yet."
+      }
     </>
   );
 }
-
