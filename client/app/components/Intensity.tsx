4324b13e068b188db6ad3aa34e962530201747d5
nikobatzi1990 <nicole.boci@gmail.com>
2023-12-26 12:40:09 +0900

    feat: create intensity level component

---
+++ client/app/components/Intensity.tsx
@@ -0,0 +1,44 @@
+import Input from "./Input";
+
+export default function Intensity() {
+  return (
+    <>
+      <p>Meltdown Intensity</p>
+      <Input 
+        id='intensity_one'
+        name='intensity'
+        text='1'
+        type='radio'
+        value='1' 
+      />
+        <Input 
+        id='intensity_two'
+        name='intensity'
+        text='2'
+        type='radio'
+        value='2' 
+      />
+      <Input 
+        id='intensity_three'
+        name='intensity'
+        text='3'
+        type='radio'
+        value='3' 
+      />
+      <Input 
+        id='intensity_four'
+        name='intensity'
+        text='4'
+        type='radio'
+        value='4' 
+      />
+      <Input 
+        id='intensity_five'
+        name='intensity'
+        text='5'
+        type='radio'
+        value='5' 
+      />
+    </>
+  );
+}
